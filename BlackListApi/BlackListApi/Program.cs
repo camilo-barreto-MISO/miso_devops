7d7ce36f021ecc10830e85d27adc4703f0c36f35
Camilo Barreto <camilo.barreto@sinco.com.co>
2024-11-30 12:46:02 -0500

    Se agrega telemetria con new relic y open telemetry

--- BlackListApi/BlackListApi/Program.cs
+++ BlackListApi/BlackListApi/Program.cs
@@ -9,44 +9,80 @@ using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.EntityFrameworkCore;
 using NSwag;
 using NSwag.Generation.Processors.Security;
+using OpenTelemetry;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
 
 var builder = WebApplication.CreateSlimBuilder(args);
 
 // We build the connection string based on the env variables DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME
 var connectionString = Environment.GetEnvironmentVariable("DB_CONNECTION_STRING") ??
-					   builder.Configuration.GetConnectionString("DefaultConnection");
+                       builder.Configuration.GetConnectionString("DefaultConnection");
 
 builder.Services.AddDbContext<EmailsDbContext>(options =>
-	options.UseNpgsql(connectionString, o =>
-	{
-		o.EnableRetryOnFailure(5);
-	})
-	.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking));
+    options.UseNpgsql(connectionString, o => { o.EnableRetryOnFailure(5); })
+        .UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking));
 
 builder.Services.AddMediator(options =>
-	options.ServiceLifetime = ServiceLifetime.Scoped);
+    options.ServiceLifetime = ServiceLifetime.Scoped);
 builder.Services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
 builder.Services.AddTransient(typeof(IPipelineBehavior<,>), typeof(RequestValidationBehavior<,>));
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddOpenApiDocument(config =>
 {
-	config.DocumentName = "BlackListAPI";
-	config.Title = "BlackListAPI v1";
-	config.Version = "v1";
-	
-	config.AddSecurity("ApiKey", new OpenApiSecurityScheme
-	{
-		Type = OpenApiSecuritySchemeType.ApiKey,
-		Name = "X-Api-Key",
-		In = OpenApiSecurityApiKeyLocation.Header,
-		Description = "API Key",
-		Scheme = "ApiKeyScheme",
-		
-	});
-	
-	config.OperationProcessors.Add(new OperationSecurityScopeProcessor("ApiKey"));
+    config.DocumentName = "BlackListAPI";
+    config.Title = "BlackListAPI v1";
+    config.Version = "v1";
+
+    config.AddSecurity("ApiKey", new OpenApiSecurityScheme
+    {
+        Type = OpenApiSecuritySchemeType.ApiKey,
+        Name = "X-Api-Key",
+        In = OpenApiSecurityApiKeyLocation.Header,
+        Description = "API Key",
+        Scheme = "ApiKeyScheme",
+    });
+
+    config.OperationProcessors.Add(new OperationSecurityScopeProcessor("ApiKey"));
 });
+
+// Add opentelemetry
+
+var otlServiceName = Environment.GetEnvironmentVariable("DB_CONNECTION_STRING") ?? "BlackListAPI";
+
+var resourceBuilder = ResourceBuilder.CreateDefault()
+    .AddService(otlServiceName);
+
+builder.Services.AddOpenTelemetry()
+    .UseOtlpExporter()
+    .WithTracing(tracerProviderBuilder =>
+    {
+        tracerProviderBuilder
+            .AddAspNetCoreInstrumentation()
+            .AddHttpClientInstrumentation()
+            .AddEntityFrameworkCoreInstrumentation()
+            .SetResourceBuilder(resourceBuilder);
+    })
+    .WithMetrics(metricProviderBuilder =>
+    {
+        metricProviderBuilder
+            .AddAspNetCoreInstrumentation()
+            .AddHttpClientInstrumentation()
+            .SetResourceBuilder(resourceBuilder);
+    });
+
+// Configure the OpenTelemetry SDK for logs
+builder.Logging.ClearProviders();
+builder.Logging.AddOpenTelemetry(options =>
+{
+    options.IncludeFormattedMessage = true;
+    options.ParseStateValues = true;
+    options.IncludeScopes = true;
+    options.SetResourceBuilder(resourceBuilder);
+});
+
 
 var app = builder.Build();
 app.UseMiddleware<CustomApiKeyAuthenticationMiddleware>();
@@ -54,11 +90,11 @@ app.UseCustomExceptionHandler();
 app.UseOpenApi();
 app.UseSwaggerUi(config =>
 {
-	config.DocumentTitle = "BlackListAPI";
-	config.Path = "/swagger";
-	config.DocumentPath = "/swagger/{documentName}/swagger.json";
-	config.DocExpansion = "list";
-	config.PersistAuthorization = true;
+    config.DocumentTitle = "BlackListAPI";
+    config.Path = "/swagger";
+    config.DocumentPath = "/swagger/{documentName}/swagger.json";
+    config.DocExpansion = "list";
+    config.PersistAuthorization = true;
 });
 
 
@@ -71,25 +107,24 @@ dbContext.Database.Migrate();
 
 app.UseForwardedHeaders(new ForwardedHeadersOptions
 {
-	ForwardedHeaders = ForwardedHeaders.XForwardedFor |
-					   ForwardedHeaders.XForwardedProto
+    ForwardedHeaders = ForwardedHeaders.XForwardedFor |
+                       ForwardedHeaders.XForwardedProto
 });
 
 app.MapPost("/blacklist", async (IMediator mediator, BlackListEmailCommand command, HttpContext httpContext) =>
 {
-	var ipAddress = httpContext.Connection.RemoteIpAddress?.ToString();
-	command.SourceIp = ipAddress;
+    var ipAddress = httpContext.Connection.RemoteIpAddress?.ToString();
+    command.SourceIp = ipAddress;
 
-	var response = await mediator.Send(command);
-	return Results.Created($"/blacklist/{response.Message}", response);
+    var response = await mediator.Send(command);
+    return Results.Created($"/blacklist/{response.Message}", response);
 });
 
 app.MapGet("/blacklist/{email}", async (IMediator mediator, string email) =>
 {
-	var response = await mediator.Send(new GetEmail { Email = email });
-	return Results.Ok(response);
+    var response = await mediator.Send(new GetEmail { Email = email });
+    return Results.Ok(response);
 });
-
 
 
 app.Run();
